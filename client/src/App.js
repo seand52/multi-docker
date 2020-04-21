8dc9376ee58d6cce6e1252aaf9c9f35132cedcd9
Sean Daryanani <sean.daryanani@nb-sdaryanani.local>
2020-04-21 15:43:08 +0200

    Boot project with docker-compose setup

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import React, { Component } from 'react';
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Fib from './Fib';
+import logo from './logo.svg';
+import OtherPage from './OtherPage';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Go to the other page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
